59f7b45249a5a497b5a8837199427a8e20513639
Akash <hafejmohammadakash@gmail.com>
2025-04-04 11:28:55 -0600

    try catch finaly

---
+++ even-loop-6.js
@@ -0,0 +1,16 @@
+function checkAge() {
+    const ageFeild = document.getElementById("age");
+    const ageText = ageFeild.value;
+    const errorTeg = document.getElementById("error")
+    
+    try{
+        console.log(bBariya)
+
+    }catch(err){
+        console.log("ERROR", err)
+        errorTeg.innerHTML = `<p>sumting is wrong!</p>`
+    }finally{
+        console.log("all done")
+    }
+    console.log(11111)
+}
