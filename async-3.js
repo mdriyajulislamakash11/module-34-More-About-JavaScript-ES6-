c3ee465f21339470775058ed18a2e6a0e0b53d6c
Akash <hafejmohammadakash@gmail.com>
2025-04-04 07:33:54 -0600

    first commit

---
+++ async-3.js
@@ -0,0 +1,17 @@
+// 34-3 Single-threaded, Asynchronous vs Synchronous
+
+console.log(1);
+console.log(2);
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// setTimeout(() => {
+//   console.log("no secuence");
+// }, 4000);
+
+
+console.log(4);
+console.log(5);
+console.log(6);
