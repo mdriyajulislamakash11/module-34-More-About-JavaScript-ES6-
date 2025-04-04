fd82a5d571a122f22d525b85956fe46d0e9b73ad
Akash <hafejmohammadakash@gmail.com>
2025-04-04 08:36:22 -0600

    fatching with 3 way

--- async-4.js
+++ async-4.js
@@ -12,7 +12,32 @@ const myLoader = () => {
   });
 };
 
-
 myLoader()
-.then(data => console.log("resolved data: ", data))
-.catch( err => console.log("rejected data: ", err))
+  .then((data) => console.log("resolved data: ", data))
+  .catch((err) => console.log("rejected data: ", err));
+
+
+
+// fetch 3 vabe kora jay
+
+fetch("url")
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+// reguer function
+async function loadData() {
+  const res = await fetch("url");
+  const data = await res.json();
+  console.log(data);
+}
+loadData();
+
+// arrow function
+
+const myLoadData = async () => {
+  const res = await fetch("url");
+  const data = await res.json();
+  console.log(data);
+};
+myLoadData()
